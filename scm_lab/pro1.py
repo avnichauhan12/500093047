ae81ce97e94b28d14465c55bba1c2255ee211673
avnichauhan <500093047@upes.stu.ac.in>
2022-09-21 19:11:42 +0530

    second commit

--- scm_lab/pro1.py
+++ scm_lab/pro1.py
@@ -1,2 +1,7 @@
 print("myself avni chauhan")
 print("i live in haridwar")
+print("sum of two number")
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+c=a+b
+print(c)
